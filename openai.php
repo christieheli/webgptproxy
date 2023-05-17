08c7f302ce738d6c3af5430926329c5208fe10a6
christieheli <christieheli@gmail.com>
2023-05-17 14:50:41 +0800

    Update openai.php
    add embeddings  api

--- openai.php
+++ openai.php
@@ -12,15 +12,32 @@ class openai {
 		$this->_http = new http();
 	}
 
-	public function chatCompletions($prompt)
+	public function chatCompletions($prompt, $model = '')
 	{
+		$model = isset($model) && !empty($model) ? $model : 'gpt-3.5-turbo';
 		$body = [
-			'model' => 'gpt-3.5-turbo',
+			'model' => $model,
 			'messages' => json_decode($prompt),
 			'temperature' => 0.8,
 		];
 		$requestParam = json_encode($body);
 		$url = 'https://api.openai.com/v1/chat/completions';
+		$this->_http->setHeader('Content-Type', 'application/json');
+		$this->_http->setHeader('Authorization', 'Bearer ' .self::APPKEY);
+		$retStr = $this->_http->setUrl($url)->setData($requestParam)->request('post', true);
+		#var_dump($this->_http->getHttpInfo());
+		return $retStr;
+	}
+	
+	public function embeddings($input, $model = '')
+	{
+		$model = isset($model) && !empty($model) ? $model : 'text-embedding-ada-002';
+		$body = [
+			'model' => $model,
+			'messages' => $input
+		];
+		$requestParam = json_encode($body);
+		$url = 'https://api.openai.com/v1/embeddings';
 		$this->_http->setHeader('Content-Type', 'application/json');
 		$this->_http->setHeader('Authorization', 'Bearer ' .self::APPKEY);
 		$retStr = $this->_http->setUrl($url)->setData($requestParam)->request('post', true);
